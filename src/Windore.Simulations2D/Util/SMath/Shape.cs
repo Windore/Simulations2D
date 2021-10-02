0693b234d5ad8982303a5790eeac488e00a7b355
Windore <windore.wi@protonmail.com>
2021-10-02 11:57:14 +0300

    Fixed Shape.Overlaps method for ellipses

--- src/Windore.Simulations2D/Util/SMath/Shape.cs
+++ src/Windore.Simulations2D/Util/SMath/Shape.cs
@@ -31,7 +31,7 @@ namespace Windore.Simulations2D.Util.SMath
         /// <param name="width">The width of the shape</param>
         /// <param name="height">The height of the shape</param>
         /// <param name="isEllipse">Is the shape an ellipse or a rectangle.</param>
-        public Shape(Point position, double width, double height, bool isEllipse) 
+        public Shape(Point position, double width, double height, bool isEllipse)
         {
             Position = position;
             Width = width;
@@ -44,43 +44,71 @@ namespace Windore.Simulations2D.Util.SMath
         /// </summary>
         /// <param name="shape">A given shape to check for overlapping.</param>
         /// <returns>True if the current and the given shape overlap.</returns>
-        public bool Overlaps(Shape shape) 
+        public bool Overlaps(Shape shape)
         {
-            if (IsEllipse == shape.IsEllipse && IsEllipse) 
+            if (IsEllipse == shape.IsEllipse && IsEllipse)
             {
-                // Figure out if two ellipses overlap by stretching the ellipses to circles and measuring the stretched distance
-                // I have no clue if this actually works, but the tests haven't failed so far..
-                double xyRelationThis = Width / Height;
-                double xyRelationOther = shape.Width / shape.Height;
+                // Testing if two ellipses overlap is rather complicated
+                Shape first = new Shape(Position, Width, Height, IsEllipse);
+                Shape second = new Shape(shape.Position, shape.Width, shape.Height, shape.IsEllipse);
+                // First the first ellipse is placed to (0,0) and the second ellipse needs to be moved as well
+                double xChange = -first.Position.X;
+                double yChange = -first.Position.Y;
 
-                return Math.Pow((Position.X - shape.Position.X), 2) 
-                    + xyRelationOther * xyRelationThis * Math.Pow((Position.Y - shape.Position.Y), 2) 
-                    < Math.Pow(Width / 2d + shape.Width / 2d, 2);
+                first = first.Move(new Point(0, 0));
+                second = second.Move(new Point(second.Position.X + xChange, second.Position.Y + yChange));
+
+                // Scale the second ellipse to a circle.
+                double relation = second.Width / second.Height;
+                second = new Shape(new Point(second.Position.X, second.Position.Y * relation), second.Width, second.Height * relation, true);
+                // The first ellipse doesn't change it's location but it's height does change
+                first = new Shape(first.Position, first.Width, first.Height * relation, true);
+
+                // The parameterized form for the first ellipse is used to discover the "radius" towards the second ellipse
+                double alpha;
+                if (second.Position.X == 0d)
+                {
+                    alpha = Math.PI / 2d;
+                }
+                else
+                {
+                    alpha = Math.Atan(second.Position.Y / second.Position.X);
+                }
+
+                double s = Math.Sqrt(Math.Pow(first.Width / 2d * Math.Cos(alpha), 2) + Math.Pow(first.Height / 2d * Math.Sin(alpha), 2d));
+
+                // The radius of the second ellipse which now is a circle
+                double r = second.Width / 2d;
+
+                // Distance between the first and second ellipse center points:
+                double d = first.Position.DistanceTo(second.Position);
+
+                return s + r > d;
             }
-            else if (IsEllipse == shape.IsEllipse && !IsEllipse) 
+            else if (IsEllipse == shape.IsEllipse && !IsEllipse)
             {
                 return Math.Abs(Position.X - shape.Position.X) < Width / 2d + shape.Width / 2d
                     && Math.Abs(Position.Y - shape.Position.Y) < Height / 2d + shape.Height / 2d;
             }
-            else 
+            else
             {
                 Shape ellipse;
                 Shape rectangle;
 
-                if (IsEllipse) 
+                if (IsEllipse)
                 {
                     ellipse = this;
                     rectangle = shape;
                 }
-                else 
+                else
                 {
                     ellipse = shape;
                     rectangle = this;
                 }
 
-                // Same streching principle is applied here
+                // Scale the ellipse to a circle. Figuring out if a rectangle overlaps a circle is easy. The rectangle is scaled as well. 
                 double xyRelation = ellipse.Width / ellipse.Height;
-                return SMath.CircleOverlapsRectangle(ellipse.Position, ellipse.Width, rectangle.Position, rectangle.Width, rectangle.Height * xyRelation);
+                return SMath.CircleOverlapsRectangle(new Point(ellipse.Position.X, ellipse.Position.Y * xyRelation), ellipse.Width, new Point(rectangle.Position.X, rectangle.Position.Y * xyRelation), rectangle.Width, rectangle.Height * xyRelation);
             }
         }
 
@@ -89,7 +117,7 @@ namespace Windore.Simulations2D.Util.SMath
         /// </summary>
         /// <param name="newPosition">The new position of the shape.</param>
         /// <returns>The same shape, but with a new position.</returns>
-        public Shape Move(Point newPosition) 
+        public Shape Move(Point newPosition)
         {
             return new Shape(newPosition, Width, Height, IsEllipse);
         }
